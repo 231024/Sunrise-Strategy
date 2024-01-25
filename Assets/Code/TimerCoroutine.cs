1ec8ed93b9e37cda6def3ebc13e7e00dee730eb7
sunriseq26 <sunriseq26@gmail.com>
2024-01-25 14:42:49 +0300

    Running timer

--- Assets/Code/TimerCoroutine.cs
+++ Assets/Code/TimerCoroutine.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TimerCoroutine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_Text _timerTxt;
+    [SerializeField] private float _valueInSecondsForTheTimer;
+   
+    private bool _isTimerPlaying;
+    private string _timePlayingFormat;
+    private TimeSpan _timePlaying;
+ 
+    private Coroutine _coroutineTimer;
+   
+    private void Start()
     {
+        _isTimerPlaying = false;
+        _timerTxt.text = "00:00.0";
         
+        BeginTimer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (_valueInSecondsForTheTimer <= 0f)
+        {
+            _isTimerPlaying = false;
+            StopCoroutine(_coroutineTimer);
+        }
+    }
+
+    private void BeginTimer()
+    {
+        _isTimerPlaying = false;
+        if (_coroutineTimer != null) {
+            StopCoroutine(_coroutineTimer);
+        }
         
+        _isTimerPlaying = true;
+        _coroutineTimer = StartCoroutine(UpdateTimer());
+    }
+ 
+    private IEnumerator UpdateTimer()
+    {
+        while (_isTimerPlaying)
+        {
+            _valueInSecondsForTheTimer -= Time.deltaTime;
+           
+            _timePlaying = TimeSpan.FromSeconds(_valueInSecondsForTheTimer);
+            _timePlayingFormat = _timePlaying.ToString("mm':'ss'.'f");
+            _timerTxt.text = _timePlayingFormat;
+           
+            yield return null;
+        }
     }
 }
